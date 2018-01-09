25aca95b11d9366e403591d8937820dbf669dc9a
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-09 21:13:03 +0530

    Up and Running with Webpack and Webpack dev server!

---
+++ config/webpack.dev.js
@@ -0,0 +1,18 @@
+/**
+ * WebPack Config: Dev
+ */
+
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: ['./src/main.js'],
+  },
+  output: {
+    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, '../dist/js'),
+  },
+  devServer: {
+    contentBase: 'dist',
+  },
+};
